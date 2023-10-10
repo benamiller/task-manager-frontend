e028b59f3071ad07190a14d9bb6fadfa83932301
hactuallybenji <mrbenamiller@gmail.com>
2023-10-09 17:58:07 -0700

    Adds story and makes responsive Canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,14 +1,14 @@
 const Canvas = () => {
   return (
-    <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-fuchsia-300 w-screen h-screen flex flex-wrap md:flex-nowrap justify-center items-center">
-      <div className="h-1/12 flex justify-end items-center md:w-1/6">
-        <ul>
-          <li>Create New Task</li>
-          <li>Create New Board</li>
+    <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-fuchsia-300 w-screen h-screen flex flex-wrap md:flex-nowrap justify-start items-center min-w-min">
+      <div className="flex justify-start items-center min-w-max px-10">
+        <ul className="text-center md:text-left">
+          <li>New Task</li>
+          <li>New Board</li>
         </ul>
       </div>
-      <main className="flex justify-center items-center h-5/6 w-5/6 md:w-5/6">
-        <div className="bg-primary-500 w-5/6 h-full rounded-lg"></div>
+      <main className="flex justify-center items-center h-5/6 w-5/6">
+        <div className="bg-primary-500 w-full h-full rounded-lg"></div>
       </main>
     </div>
   );
---
+++ src/stories/Canvas.stories.js
@@ -0,0 +1,12 @@
+import Canvas from "../components/Canvas.jsx";
+
+export default {
+  title: "Board/Canvas",
+  component: Canvas,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const Primary = {};
