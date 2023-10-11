3919b3cd092ebddfc64177fb79eabbdb4540338d
hactuallybenji <mrbenamiller@gmail.com>
2023-10-10 19:16:58 -0700

    Style Button component to fit main canvas

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ text }) => {
   return (
-    <button className="border-2 border-primary-100 text-primary-500 font-normal hover:bg-primary-100 hover:text-white hover:border-white py-1 px-3 mx-10 rounded-full shadow transition ease-out duration-300">
+    <button className="border-2 border-white text-white font-normal my-1 hover:bg-primary-500 hover:text-primary-100 hover:border-primary-500 py-1 px-3 mx-10 rounded-full shadow transition ease-out duration-300">
       {text}
     </button>
   );
