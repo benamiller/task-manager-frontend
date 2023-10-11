01b4712987d8876c931e092b8beeae74190f9bf9
hactuallybenji <mrbenamiller@gmail.com>
2023-10-10 19:32:22 -0700

    Adds Column component empty definition

---
+++ src/stories/Column.stories.js
@@ -0,0 +1,12 @@
+import Column from "../components/Column";
+
+export default {
+  title: "Board/Column",
+  component: Column,
+  tags: ["autodocs"],
+  paramaters: {
+    layout: "fullscreen",
+  },
+};
+
+export const Primary = {};
