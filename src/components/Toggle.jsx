8928349b28589a82dd7e83cae51c0ad34b781674
hactuallybenji <mrbenamiller@gmail.com>
2023-10-10 20:20:19 -0700

    Adds preliminary Toggle component

---
+++ src/components/Toggle.jsx
@@ -0,0 +1,15 @@
+const Toggle = ({ text }) => {
+  return (
+    <>
+      <label className="relative inline-flex items-center cursor-pointer">
+        <input type="checkbox" value="testValue" className="sr-only peer" />
+        <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+        <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+          {text}
+        </span>
+      </label>
+    </>
+  );
+};
+
+export default Toggle;
