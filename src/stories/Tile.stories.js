bae980d71814c8a8b1a5bc636accaca2cc3598e2
hactuallybenji <mrbenamiller@gmail.com>
2023-10-10 19:49:04 -0700

    Adds Tile component definition

---
+++ src/stories/Tile.stories.js
@@ -0,0 +1,16 @@
+import Tile from "../components/Tile";
+
+export default {
+  title: "Board/Tile",
+  component: Tile,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+export const Primary = {
+  args: {
+    text: "Test Tile",
+  },
+};
